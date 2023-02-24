b0ca434a36b909a93e47c95e51691cf6b75383a6
root <root@LAPTOP-VM5PGI0Q>
2023-02-24 03:36:54 -0800

    algo1

--- Algorithm_1.py
+++ Algorithm_1.py
@@ -1,1 +1,22 @@
+def find_preferred_city(city_distances, fuel, mpg):
+    max_distance = 0
+    preferred_city = None
+    num_cities = len(city_distances)
+    
+    for i in range(num_cities):
+        current_fuel = fuel[i]
+        distance_traveled = 0
+        j = i
+        while current_fuel * mpg >= city_distances[j]:
+            distance_traveled += city_distances[j]
+            current_fuel -= city_distances[j] / mpg
+            j = (j + 1) % num_cities
+            if j == i:
+                break
+        
+        if distance_traveled > max_distance:
+            max_distance = distance_traveled
+            preferred_city = i
+    
+    return preferred_city
 
